55c9cc9236dc3f49a64adbc72989a8a8582843fc
Oren Leiman <oleiman@neutral.io>
2014-07-18 13:46:27 -0700

    updates ConfigAction to handle object properties
    Signed-off-by: Oren Leiman <oleiman@neutral.io>

--- src/nio_instance/actions/config.py
+++ src/nio_instance/actions/config.py
@@ -18,12 +18,11 @@ class ConfigAction(Action):
                                   "%s_types" % self.args.resource, '')
         
     def perform(self, data=None):
-        exclude = ['name', 'sys_metadata']
+        exclude = ['name', 'sys_metadata', 'mappings', 'execution']
         data = {}
 
         # TODO: boldly unsafe - mostly the json deser
         resource = requests.get(self.urls[0], auth=self.auth).json()
-        print(resource)
         resource_type = resource['type']
         type_url = self._create_types_url()
         all_resources = requests.get(type_url, auth=self.auth).json()
@@ -33,29 +32,45 @@ class ConfigAction(Action):
             detail = resource_props[prop]
             _type = detail['type']
             if not detail.get('readonly', False):
-                val = self._process_property(prop, detail)
-                data[prop] = val if val != '' else resource[prop]
+                val = self._process_property(prop, detail, resource[prop])
+                data[prop] = val
 
         super().perform(data)
 
-    def _process_property(self, name, detail, prompt="{0} ({1}):"):
-        result = None
+    def _process_property(self, name, detail, original, prompt="\n{0} ({1}): "):
+        result = ''
         _type = detail['type']
+
+        # TODO: define semantics for adding to lists
         if _type == 'list':
-            # TODO: define semantics for adding to lists
             return result
+
+        # handle an object property gracefully
         elif _type == 'object':
-            print(prompt.format(name, _type))
             result = {}
             template = detail['template']
+            print(prompt.format(name, _type))
+            
+            # prompt for a value for each of the sub-properties
             for key in template:
-                result[key] = self._process_property(key, template[key],
-                                                     "+->{0} ({1}):")
-        else:
-            result = input(prompt.format(name, _type)) 
-            if _type == 'int':
-                result = int(result)
-            elif _type == 'bool':
-                result = True if re.match(r'[tT]', result) else False
+                result[key] = self._process_property(
+                    key, template[key], original[key], "+->{0} ({1}): "
+                )
 
+        # otherwise, just a lonely field
+        else:
+            result = self._process_field(name, _type, original, prompt)
+
+        return result
+        
+    # if a field is left blank, leave the property's value unchanged
+    def _process_field(self, name, _type, original, prompt):
+        result = input(prompt.format(name, _type)) 
+        if result == '':
+            result = original
+            print('Using current value:', result)
+        elif _type == 'int':
+            result = int(result)
+        elif _type == 'bool':
+            result = True if re.match(r'[tT]', result) else False
         return result

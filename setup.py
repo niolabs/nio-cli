003bb01c48d9d39eaa758b148eec1e905e538521
Oren Leiman <oleiman@neutral.io>
2014-07-24 12:24:34 -0700

    Add setuptools based install script
    Signed-off-by: Oren Leiman <oleiman@neutral.io>

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+    name='nio-cli',
+    version="0.1",
+    packages=find_packages(
+        where='src'
+    ),
+    package_dir = {'': 'src'},
+
+    entry_points={
+        'console_scripts': [
+            'nio-instance=nio_instance.main:_nio_instance_main'
+        ]
+    },
+    author="Oren Leiman",
+    author_email="oren@neutral.io",
+    description="Command line tools for NIO",
+    license="Apache",
+    url="docs.n.io/en/latest/nio_cli.html"
+)

b0a4999f44cb10c5ade134088b645c3834a94041
Chris Brackert <cbrackert@gmail.com>
2015-05-13 19:34:31 -0700

    Fix shutdown command

--- nio_cli/main.py
+++ nio_cli/main.py
@@ -50,7 +50,7 @@ def _nio_instance_main():
     cmd_parser = subparsers.add_parser('command', aliases=['co'])
     cmd_parser.set_defaults(action=CommandAction)
     cmd_parser.add_argument('command', type=str)
-    cmd_parser.add_argument('service', type=str)
+    cmd_parser.add_argument('service', type=str, nargs='?', default=None)
     cmd_parser.add_argument('block', nargs='?', default='')
     cmd_parser.add_argument('--auto', '-a', action='store_true')
     cmd_parser.add_argument('--args', type=argument, nargs='*', default=[])

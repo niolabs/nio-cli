a944e45a3243e333cf85af66f93f82aad922fb71
Chris Brackert <cbrackert@gmail.com>
2016-03-08 18:30:08 -0800

    Add 'list' command

--- nio_cli/commands/list.py
+++ nio_cli/commands/list.py
@@ -1,11 +1,22 @@
-"""The list command."""
-
-
+import json
+import requests
 from .base import Base
 
 
 class List(Base):
-    """ Get basic nio info """
+
+    def __init__(self, options, *args, **kwargs):
+        super().__init__(options, *args, **kwargs)
+        self._resource = 'services' if self.options['services'] else 'blocks'
+        self._resource_name = \
+            self.options['<service-name>'] if self.options['services'] else \
+            self.options['<block-name>'] if self.options['blocks'] else \
+            ""
 
     def run(self):
-        print("You're using nio: list")
+        response = requests.get(self._base_url.format(self._resource),
+                                auth=self._auth)
+        try:
+            [print(resource) for resource in response.json()]
+        except:
+            pass

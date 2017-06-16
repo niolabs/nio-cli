217743b06d7a5b5c3c99e90e39b2a0325d90daef
Dylan Orzel <dylan.orzel@gmail.com>
2017-06-16 15:02:53 -0600

    Update server command executable to be configurable

--- tests/test_command_server.py
+++ tests/test_command_server.py
@@ -5,7 +5,7 @@ from nio_cli.commands import Server
 
 class TestCommandServer(unittest.TestCase):
 
-    def test_server(self):
+    def test_server_run(self):
         """The 'server' command executes nio binary as a subprocess"""
         with patch('nio_cli.commands.server.subprocess.Popen') as Popen:
             options = {}
@@ -13,5 +13,18 @@ class TestCommandServer(unittest.TestCase):
             options['-d'] = False
             options['--ip'] = '127.0.0.1'
             options['--port'] = 8181
+            options['--run'] = True
+            Server(options).run()
+            self.assertEqual(Popen.call_args[0][0], ['nio_run'])
+
+    def test_server_full(self):
+        """The 'server' command executes nio binary as a subprocess"""
+        with patch('nio_cli.commands.server.subprocess.Popen') as Popen:
+            options = {}
+            options['--daemon'] = False
+            options['-d'] = False
+            options['--ip'] = '127.0.0.1'
+            options['--port'] = 8181
+            options['--full'] = True
             Server(options).run()
             self.assertEqual(Popen.call_args[0][0], ['nio_full'])

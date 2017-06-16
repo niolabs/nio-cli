01d171b9e1e33da1416b4816f463cf915307926a
Dylan Orzel <dylan.orzel@gmail.com>
2017-06-16 15:07:04 -0600

    Add nio_run as default server executable, Add test for default

--- tests/test_command_server.py
+++ tests/test_command_server.py
@@ -6,7 +6,8 @@ from nio_cli.commands import Server
 class TestCommandServer(unittest.TestCase):
 
     def test_server_run(self):
-        """The 'server' command executes nio binary as a subprocess"""
+        """This will execute nio as a subprocess with executable name nio_run
+        """
         with patch('nio_cli.commands.server.subprocess.Popen') as Popen:
             options = {}
             options['--daemon'] = False
@@ -18,7 +19,8 @@ class TestCommandServer(unittest.TestCase):
             self.assertEqual(Popen.call_args[0][0], ['nio_run'])
 
     def test_server_full(self):
-        """The 'server' command executes nio binary as a subprocess"""
+        """This will execute nio as a subprocess with executable name nio_full
+        """
         with patch('nio_cli.commands.server.subprocess.Popen') as Popen:
             options = {}
             options['--daemon'] = False
@@ -28,3 +30,16 @@ class TestCommandServer(unittest.TestCase):
             options['--full'] = True
             Server(options).run()
             self.assertEqual(Popen.call_args[0][0], ['nio_full'])
+
+    def test_server_default(self):
+        """This will execute nio as a subprocess with executable name nio_run
+        as default when no arguments are specified
+        """
+        with patch('nio_cli.commands.server.subprocess.Popen') as Popen:
+            options = {}
+            options['--daemon'] = False
+            options['-d'] = False
+            options['--ip'] = '127.0.0.1'
+            options['--port'] = 8181
+            Server(options).run()
+            self.assertEqual(Popen.call_args[0][0], ['nio_run'])

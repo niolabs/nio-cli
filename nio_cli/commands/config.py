a013cb044475b6ab9cea75570c86b43c0dd98f16
Jake Snell <jsnell@n.io>
2017-06-27 15:47:27 -0600

    Update config command

--- nio_cli/commands/config.py
+++ nio_cli/commands/config.py
@@ -1,11 +1,27 @@
-"""The config command."""
-
-
 from .base import Base
-
+import requests
 
 class Config(Base):
     """ Get basic nio info """
 
+    def __init__(self, options, *args, **kwargs):
+        super().__init__(options, *args, **kwargs)
+        self._resource = 'services' if self.options['services'] else 'blocks'
+        self._resource_name = \
+            self.options['<service-name>'] if self.options['services'] else \
+            self.options['<block-name>'] if self.options['blocks'] else \
+            ""
+
     def run(self):
-        print("You're using nio: config")
+        response = requests.get(
+            self._base_url.format(
+                '{}/{}'.format(self._resource, self._resource_name)),
+            auth=self._auth)
+        try:
+            config = response.json()
+            print(requests.get(
+                self._base_url.format(self._resource),
+                json=config,
+                auth=self._auth).json())
+        except Exception as e:
+            print(e)

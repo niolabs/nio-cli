a92fa51f20b6971c84cdfaea1f34c7374636d4d4
Chris Brackert <cbrackert@gmail.com>
2017-07-19 17:21:29 -0600

    Refactor and improve unit tests

--- nio_cli/commands/list.py
+++ nio_cli/commands/list.py
@@ -8,10 +8,6 @@ class List(Base):
     def __init__(self, options, *args, **kwargs):
         super().__init__(options, *args, **kwargs)
         self._resource = 'services' if self.options['services'] else 'blocks'
-        self._resource_name = \
-            self.options['<service-name>'] if self.options['services'] else \
-            self.options['<block-name>'] if self.options['blocks'] else \
-            ""
 
     def run(self):
         response = requests.get(self._base_url.format(self._resource),

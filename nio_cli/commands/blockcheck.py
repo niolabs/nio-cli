3087b0048c55191a90afee35232e3c930b95cd91
cowleyk <cowley.kevin.l@gmail.com>
2017-08-15 16:32:33 -0600

    Check pep8, spec, readme, release

---
+++ nio_cli/commands/blockcheck.py
@@ -0,0 +1,125 @@
+import json, os, re
+import subprocess
+from collections import defaultdict
+
+from .base import Base
+
+
+
+
+class BlockCheck(Base):
+
+    # This should be run from inside the block dir
+
+    def __init__(self, options, *args, **kwargs):
+        super().__init__(options, *args, **kwargs)
+        self._block = os.getcwd().split('/')[-1]
+        self.all_contents = os.listdir('.')
+        self.block_files = [f for f in self.all_contents if 'block.py' in f and 'base' not in f]
+        self.blocks_in_spec = []
+        self.keys = [
+            'version',
+            'description',
+            'properties',
+            'inputs',
+            'outputs',
+            'commands'
+        ]
+
+    def run(self):
+        # OK to assume each block has a corresponding file and each file only has one block?
+        # OK that everything uses `print` statements?
+
+        self.print_check('PEP8')
+        self.check_pep8()
+
+        self.print_check('spec.json')
+        self.check_spec()
+
+        self.print_check('README.md')
+        self.check_readme()
+
+        self.print_check('release.json')
+        self.check_release()
+
+        self.print_check('version')
+        # TODO: version should match other versions (set as global var?)
+        # check that all versions sync'd; block, release, spec
+        # should this be done along the way? (ie read version from block file, set and check along way?)
+
+        self.print_check('class and file name')
+        # check classes for camel case, blocks for snake case
+
+    def check_pep8(self):
+
+        shell_pep8 = 'pep8 .'
+        subprocess.call(shell_pep8, shell=True)
+        print('')
+        # TODO: control output of pep8 command
+            # would be nice to know if no issues was present
+
+    def check_spec(self):
+
+        if os.path.exists('spec.json'):
+            with open('spec.json') as f:
+                spec_dict = json.load(f)
+                self.blocks_in_spec = [k.split('/')[1] for k,v in spec_dict.items()]
+
+            if len(self.blocks_in_spec) > len(self.block_files):
+                print('There\'s extra blocks in the spec file')
+            if len(self.blocks_in_spec) < len(self.block_files):
+                print('Not all blocks are in the spec file')
+
+            for block in self.blocks_in_spec:
+
+                for key in self.keys:
+                    if key not in [k for k in spec_dict['nio/' + block]]:
+                        print('{} block is missing {}'.format(block, key))
+
+                for key in ['version', 'description', 'properties']:
+                    if not spec_dict['nio/' + block][key] or spec_dict['nio/' + block][key] == '':
+                        print('Please fill in the {}'.format(key))
+
+                for prop,val in spec_dict['nio/' + block]['properties'].items():
+                    if val['description'] == '':
+                        print('Please fill in the description for the "{}" property in the {} block'.format(prop, block))
+
+        else:
+            print('Please run `nio buildspec {}` from the project directory'.format(self._block))
+        print('')
+
+    def check_readme(self):
+        # TODO: Readme has everything for each block
+
+        if os.path.exists('README.md'):
+            with open('README.md') as f:
+                lines = [l.rstrip() for l in f.readlines()]
+            for block in self.blocks_in_spec:
+                if block not in lines:
+                    print('Please add the {} block to the README')
+            for key in ['Properties', 'Inputs', 'Outputs', 'Commands', 'Dependencies']:
+                if key not in lines:
+                    print('Please add a {} section to the README'.format(key))
+        else:
+            print('Please run `nio buildreadme` as long as the spec.json file is complete')
+        print('')
+
+    def check_release(self):
+        # TODO
+            # Release has language, url, version
+            # for each block
+        if os.path.exists('release.json'):
+            with open('release.json') as f:
+                release_dict = json.load(f)
+
+        for block in self.blocks_in_spec:
+            if 'nio/' + block not in release_dict:
+                print('Please add {} block to release.json'.format(block))
+            for key in ['url', 'version', 'language']:
+                if key not in release_dict['nio/' + block] or release_dict['nio/' + block][key] == '':
+                    print('Please add a {} to the {} block'.format(key, block))
+
+        print('')
+
+    def print_check(self, check):
+        print('Checking {} formatting ...'.format(check))

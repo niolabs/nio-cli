d2468a872c876a5e042b9d0fc3e240e05cacec6e
Tyler Lugger <tlugger@n.io>
2017-10-18 14:06:24 -0600

    nioinnovation -> niolabs

--- nio_cli/cli.py
+++ nio_cli/cli.py
@@ -36,7 +36,7 @@ Examples:
 
 Help:
   For help using this tool, please open an issue on the Github repository:
-  https://github.com/nioinnovation/nio-cli
+  https://github.com/niolabs/nio-cli
 """
 
 

b208a8a905410213ebfdd96c3be34fd3b77c58ed
Tyler Lugger <tlugger@n.io>
2018-01-05 14:25:31 -0700

    add config project option

--- nio_cli/commands/config.py
+++ nio_cli/commands/config.py
@@ -6,19 +6,27 @@ class Config(Base):
 
     def __init__(self, options, *args, **kwargs):
         super().__init__(options, *args, **kwargs)
-        self._resource = 'services' if self.options['services'] else 'blocks'
+        self._resource = 'services' if self.options['services'] else \
+            'blocks' if self.options['blocks'] else \
+            'project'
         self._resource_name = \
             self.options['<service-name>'] if self.options['services'] else \
             self.options['<block-name>'] if self.options['blocks'] else \
             ""
 
     def run(self):
-        response = requests.get(
-            self._base_url.format(
-                '{}/{}'.format(self._resource, self._resource_name)),
-            auth=self._auth)
-        try:
-            config = response.json()
-            print(config)
-        except Exception as e:
-            print(e)
+        if self._resource == 'project':
+            print("project")
+            # Project config stuff
+
+        # Retrieve configuration
+        else:
+            response = requests.get(
+                self._base_url.format(
+                    '{}/{}'.format(self._resource, self._resource_name)),
+                auth=self._auth)
+            try:
+                config = response.json()
+                print(config)
+            except Exception as e:
+                print(e)

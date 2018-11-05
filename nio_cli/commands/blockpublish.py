b2497755b27ec96e4fae489b6c5a1ef116bfdd00
mattdodge <matt@n.io>
2018-11-05 09:29:31 -0800

    Include release info when publishing block

--- nio_cli/commands/blockpublish.py
+++ nio_cli/commands/blockpublish.py
@@ -11,10 +11,12 @@ class BlockPublish(Base):
 
     def run(self):
         spec = self._fetch_spec()
+        release = self._fetch_release()
         if self.options.get('--dry-run'):
             pprint(spec)
             return
         self._publish_spec(spec)
+        self._publish_release(release)
 
     def _fetch_spec(self):
         """ Fetches the spec.json and fills in details
@@ -28,7 +30,6 @@ class BlockPublish(Base):
         output_spec = {}
         with open('spec.json', 'r') as f:
             spec = json.load(f)
-        print(spec)
         for block_name, block_spec in spec.items():
             from_python = block_spec.pop('from_python', None)
             from_readme = block_spec.pop('from_readme', None)
@@ -45,28 +46,50 @@ class BlockPublish(Base):
             output_spec[block_name] = block_spec
         return output_spec
 
+    def _fetch_release(self):
+        with open('release.json', 'r') as f:
+            release = json.load(f)
+        return release
+
     def _publish_spec(self, spec):
         # Loop through the specs, we can only publish one at a time
-        access_token = self.options.get('--api-token') or \
-            getenv('API_ACCESS_TOKEN')
-        if not access_token:
-            print("ERROR: No access token specified, use the --api-token "
-                  "argument or the API_ACCESS_TOKEN env var")
-            return
         for block_name, block_spec in spec.items():
             print("Publishing version {} of {}".format(
                 block_spec.get('version'), block_name))
-            resp = requests.post(
-                (self.options.get('--api-url') or
-                    'https://api.n.io/v1/block/specification'),
-                headers={
-                    'Authorization': 'bearer {}'.format(access_token),
-                },
-                json={block_name: block_spec}
-            )
-            try:
-                resp.raise_for_status()
-                print(resp.json().get('message', 'No message received'))
-            except Exception:
-                print("ERROR: {}".format(
-                    resp.json().get('message', 'Unknown error')))
+            resp_json = self._make_request('specification', {
+                block_name: block_spec
+            })
+            print(resp_json.get('message', 'No message received'))
+
+    def _publish_release(self, release):
+        for block_name, release_info in release.items():
+            print("Publishing release {} of {}".format(
+                release_info.get('version'), block_name))
+            resp_json = self._make_request('release', {
+                block_name: release_info
+            })
+            print(resp_json.get('message', 'No message received'))
+
+    def _get_token(self):
+        access_token = self.options.get('--api-token') or \
+            getenv('API_ACCESS_TOKEN')
+        if not access_token:
+            raise RuntimeError("No access token specified, use the --api-token"
+                               " argument or the API_ACCESS_TOKEN env var")
+        return access_token
+
+    def _make_request(self, path, body):
+        resp = requests.post("{}/{}".format(
+            self.options.get('--api-url') or 'https://api.n.io/v1/block',
+            path),
+            headers={
+                'Authorization': 'bearer {}'.format(self._get_token()),
+            },
+            json=body,
+        )
+        try:
+            resp.raise_for_status()
+            return resp.json()
+        except Exception:
+            raise RuntimeError("ERROR: {}".format(
+                resp.json().get('message', 'Unknown error')))
